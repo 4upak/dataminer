c2a058b2380473a8af9ccf96ef27eabb6f644533
4upak <serg.chupak@gmail.com>
2021-11-15 23:33:26 +0200

    models directory

--- database.py
+++ database.py
@@ -9,8 +9,9 @@ db_password = 'Mm&6ry32+B'
 db_name = 'amnstrld_parsing'
 db_port = '3306'
 
-url = 'mysql://{0}:{1}@{2}:{3}'.format(db_user, db_password, db_host, port)
-engine = create_engine(url)
+#url = 'mysql://{0}:{1}@{2}:{3}'.format(db_user, db_password, db_host, port)
+url = f'mysql+mysqlconnector://{db_user}:{db_password}@{db_host}/{db_name}'
+engine = create_engine(url,echo=True)
 
 Session = sessionmaker(bind=engine)
 Base = declarative_base()
